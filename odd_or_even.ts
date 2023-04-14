af6f3638edffb96315b41314c5db13d2a359aea0
Nande03 <nandetoyi@yahoo.com>
2023-04-14 10:58:51 +0200

    odd or even

---
+++ odd_or_even.ts
@@ -0,0 +1,13 @@
+function oddOrEven(num1: number) 
+{
+    let result = num1%2;
+    let isOdd = true;
+
+    if (result == 0)
+    {
+        isOdd = false;
+    }
+    else isOdd = true;
+}
+
+oddOrEven(32)
